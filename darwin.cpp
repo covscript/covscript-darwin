0ecbaf04ce75c5a1da2c68e94d568301478bb374
mikecovlee <mikecovlee@163.com>
2018-08-13 20:51:42 +0800

    适配新版本

--- darwin.cpp
+++ darwin.cpp
@@ -30,9 +30,9 @@ static cs::extension darwin_ext;
 static cs::extension darwin_ui_ext;
 static cs::extension darwin_core_ext;
 static cs::extension darwin_drawable_ext;
-static cs::extension_t darwin_ui_ext_shared = cs::make_shared_extension(darwin_ui_ext);
-static cs::extension_t darwin_core_ext_shared = cs::make_shared_extension(darwin_core_ext);
-static cs::extension_t darwin_drawable_ext_shared = cs::make_shared_extension(darwin_drawable_ext);
+static cs::extension_t darwin_ui_ext_shared = cs::make_shared_namespace(darwin_ui_ext);
+static cs::extension_t darwin_core_ext_shared = cs::make_shared_namespace(darwin_core_ext);
+static cs::extension_t darwin_drawable_ext_shared = cs::make_shared_namespace(darwin_drawable_ext);
 namespace cs_impl {
 	template<>
 	cs::extension_t &get_ext<darwin::pixel>()
