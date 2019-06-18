153110340f66b1786e6d6737bfc1cda734b0b261
imkiva <libkernelpanic@gmail.com>
2019-06-18 23:55:35 +0800

    cmake: update cmake for auto build

--- make.bat
+++
@@ -1,4 +0,0 @@
-@mkdir build
-@cd build
-@mkdir imports
-@g++ -std=c++14 -I%CS_DEV_PATH%\include -I..\include -shared -static -fPIC -s -O3 ..\darwin.cpp -o .\imports\darwin.cse -L%CS_DEV_PATH%\lib -lcovscript
